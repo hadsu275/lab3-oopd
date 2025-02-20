e3b67e9c8b70ab9cfc7c57a719af10fbfd0df718
emilru1 <emilru@chalmers.se>
2025-02-20 19:34:39 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -2,7 +2,7 @@ import java.awt.*;
 
 public class Volvo240 extends PersonalCar {
 
-    public final static double trimFactor = 1.25;
+    private final static double trimFactor = 1.25;
 
     public Volvo240(){
         super(4, 100, "Volvo240", Color.black );
